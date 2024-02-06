bbd5b2fef6992ce24a23c97ed971b59d13149b74
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:35:53 +0530

    DateConversion.java

--- src/dateConversion/DateConversion.java
+++ src/dateConversion/DateConversion.java
@@ -5,18 +5,21 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class DateConversion {
+	private static final Logger log = Logger.getLogger(DateConversion.class.getName());
+
 	/**
 	 * @param args In this DateConverter Firstly the date is been taken as a user
 	 *             input DateTimeFormatter is used to format the way of writing Then
-	 *             the user input is parsed to LocalDateTime
-	 *             To get the unix time toEpochsecond() is used
+	 *             the user input is parsed to LocalDateTime To get the unix time
+	 *             toEpochsecond() is used
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter date and time (dd/MM//yyyy HH:mm : )");
+		log.info("Enter date and time (dd/MM/yyyy HH:mm): ");
 		String userInput = sc.nextLine();
 
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
@@ -29,22 +32,20 @@ public class DateConversion {
 
 		long unixTimestamp = istTimestamp.toEpochSecond();
 
-		System.out.println("Unix Timestamp: " + unixTimestamp);
+		log.info("Unix Timestamp: " + unixTimestamp);
 
 		ZoneId utcZone = ZoneId.of("UTC");
 
 		ZonedDateTime utcTimestamp = istTimestamp.withZoneSameInstant(utcZone);
 
-		System.out.println("UTC Timestamp: " + utcTimestamp.format(formatter));
+		log.info("UTC Timestamp: " + utcTimestamp.format(formatter));
 
 		// Convert back to local timestamp in IST
 		ZonedDateTime convertedIstTimestamp = utcTimestamp.withZoneSameInstant(istZone);
 
 		// Print local timestamp in IST
-		System.out.println("Converted IST Timestamp: " + convertedIstTimestamp.format(formatter));
+		log.info("Converted IST Timestamp: " + convertedIstTimestamp.format(formatter));
 
 		sc.close();
-
 	}
-
 }
